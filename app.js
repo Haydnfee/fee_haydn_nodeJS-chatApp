6009bcfe355887d8f9f9fa050f25535f68dd5a9c
Haydnfee <31936761+Haydnfee@users.noreply.github.com>
2018-02-07 23:11:58 -0500

    initial

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express'); //just like an includes in php
+const app = express();
+const io = require('socket.io')();
+
+app.use(express.static('public'));
+
+//set up routes
+app.use(require('./routes/index'));
+app.use(require('./routes/contact'));
+app.use(require('./routes/users'));
+
+
+const server = app.listen(3000, () => {
+  console.log('app running on port 3000!');
+});
+
+io.attach(server);
+
+io.on('connection',(socket)=>{
+  console.log('a user has connected!');
+  io.emit('chat message',{for: 'everyone', message : `${socket.id} is here!`});
+
+  //handles message sent from user
+  socket.on('chat message', msg =>{
+    io.emit('chat message',{for: 'everyone', message : msg});
+  });
+
+  socket.on('disconnect',()=>{
+    console.log('a user has disconnected!');
+
+    io.emit('disconnect message', `${socket.id} has left the building`);
+  });
+
+});
